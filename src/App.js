8cdbdd8c59fdb9090943ef61817f854cd65f2c6d
sosajseba <sosajseba@gmail.com>
2023-01-15 23:47:58 -0300

    feat: anonymous login with display name

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import { auth } from "./firebase/firebase";
+import { signInAnonymously, updateProfile } from 'firebase/auth';
+import { useState } from 'react';
 
 function App() {
+
+  const [userName, setUserName] = useState("");
+
+  function createRoom() {
+    signInAnonymously(auth)
+      .then(() => {
+        console.log("Signed in..")
+        updateProfile(auth.currentUser, {
+          displayName: userName
+        }).then(() => {
+          console.log("Profile updated..", userName)
+        }).catch((error) => {
+          console.log(error.code, error.message)
+        });
+      })
+      .catch((error) => {
+        console.log(error.code, error.message)
+      });
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        ELIGE UN APODO
+        <input type='text' value={userName} onChange={event => setUserName(event.target.value)} />
+        <button onClick={createRoom}>CREAR SALA</button>
       </header>
     </div>
   );
