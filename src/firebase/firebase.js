7e5fee7b15a48f084dc2a81bbef1cd52326fb899
sosajseba <sosajseba@gmail.com>
2023-01-15 23:47:16 -0300

    feat: add firebase config

---
+++ src/firebase/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, } from "firebase/app";
+import { getAuth, signInAnonymously } from "firebase/auth";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  appId: process.env.REACT_APP_APP_ID,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
