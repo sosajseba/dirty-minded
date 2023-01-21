3270d67ab1332c61e838da94b71310fcc1b50fe2
sosajseba <sosajseba@gmail.com>
2023-01-21 16:51:38 -0300

    feat: user can login

--- src/App.js
+++ src/App.js
@@ -1,36 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
 import { auth } from "./firebase/firebase";
-import { signInAnonymously, updateProfile } from 'firebase/auth';
-import { useState } from 'react';
+import { onAuthStateChanged, signInAnonymously, updateProfile } from 'firebase/auth';
+import { useEffect, useState } from 'react';
 
 function App() {
-
-  const [userName, setUserName] = useState("");
+  
+  const [user, setUser] = useState();
+  const [userName, setUserName] = useState('');
 
   function createRoom() {
-    signInAnonymously(auth)
-      .then(() => {
-        console.log("Signed in..")
-        updateProfile(auth.currentUser, {
-          displayName: userName
-        }).then(() => {
-          console.log("Profile updated..", userName)
-        }).catch((error) => {
-          console.log(error.code, error.message)
-        });
-      })
-      .catch((error) => {
+    if (!user.displayName) {
+      updateProfile(auth.currentUser, {
+        displayName: userName
+      }).then(() => {
+        setUser({ ...user, displayName: userName })
+        console.log("Profile updated..", userName)
+      }).catch((error) => {
         console.log(error.code, error.message)
       });
+    }
+    console.log('Name unchanged..')
   }
+
+  function handleUserStateChanged(fbUser) {
+    if (fbUser) {
+      setUser(fbUser);
+      console.log(fbUser.uid, fbUser.displayName);
+    } else {
+      signInAnonymously(auth)
+        .then(() => {
+          setUser(auth.currentUser)
+          console.log("Signed in..")
+        })
+        .catch((error) => {
+          console.log(error.code, error.message)
+        });
+    }
+  }
+
+  useEffect(() => {
+    onAuthStateChanged(auth, handleUserStateChanged)
+  }, [])
+
+  useEffect(() => {
+  }, [user])
 
   return (
     <div className="App">
       <header className="App-header">
-        ELIGE UN APODO
-        <input type='text' value={userName} onChange={event => setUserName(event.target.value)} />
-        <button onClick={createRoom}>CREAR SALA</button>
+        {user ?
+          (user.displayName ?
+            <>
+              Hola {user.displayName}!
+              <button onClick={createRoom}>CREAR SALA</button>
+            </> :
+            <>
+              Elige un apodo
+              <input type='text' value={userName} onChange={event => setUserName(event.target.value)} />
+              <button onClick={createRoom}>CREAR SALA</button>
+            </>
+          ) :
+          <>
+            Loading..
+          </>
+        }
       </header>
     </div>
   );
