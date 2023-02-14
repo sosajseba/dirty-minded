f519f470d2b08de1be95f3a81d196efa61e81794
sosajseba <sosajseba@gmail.com>
2023-02-14 20:02:16 -0300

    feat: remove unused urls

--- src/components/sockets/index.js
+++ src/components/sockets/index.js
@@ -1,7 +1,7 @@
 import io from 'socket.io-client'
 import { socketEvents } from "./events";
 
-export const socket = io.connect('http://localhost:3001');
+export const socket = io.connect(process.env.REACT_APP_SOCKET_URL);
 
 export const initSockets = ({ me, setValue, setJoined, setMe }) => {
   socketEvents({ me, setValue, setJoined, setMe });
